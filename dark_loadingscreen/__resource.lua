15c29eed63ddd9db7eb0458bc4856e73f9be3a78
catalandu <84291747+catalandu@users.noreply.github.com>
2021-06-27 20:05:25 +0200

    Add files via upload

---
+++ dark_loadingscreen/__resource.lua
@@ -0,0 +1,14 @@
+files {
+    'index.html',
+    'style.css',
+    'js/script.js',
+    'img/logo.png',
+    'music/denajpederagic.mp3',
+    'img/background.jpg'
+}
+
+loadscreen 'index.html'
+loadscreen_manual_shutdown 'yes'
+client_script 'client.lua'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
